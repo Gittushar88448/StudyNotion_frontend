68dec66793b852ee768b2e8401be3986cfaab881
Tushar <kmrtushar01@gmail.com>
2024-08-18 20:56:49 +0530

    Right section of animated section component

--- src/components/core/HomePage/AnimatedSection.jsx
+++ src/components/core/HomePage/AnimatedSection.jsx
@@ -31,6 +31,43 @@ const AnimatedSection = ({position, heading, subHeading, ctabutton1 , ctabutton2
             </div>
         </div>
 
+        {/* Right section */}
+        <div className=' flex flex-row text-base p-[20px] lg:w-[525px] relative bg-richblack-50 bg-opacity-10 )'>
+
+            <div className='absolute w-[525.95px] -top-24 -left-20 opacity-70 '>
+                <img src={gradient} alt='gradient ellipse background'></img>
+            </div>
+            
+
+            <div className='text-center flex flex-col w-[10%] text-richblack-400 font-inter font-bold z-10 '>
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+                <p>4</p>
+                <p>5</p>
+                <p>6</p>
+                <p>7</p>
+                <p>8</p>
+                <p>9</p>
+                <p>10</p>
+                <p>11</p>
+            </div>
+
+            <div className={`w-[90%] flex flex-col gap-2 font-bold font-mono ${codeColor} pr-2 z-10`}>
+                <TypeAnimation
+                    sequence={[codeblock , 6000, ""]}
+                    repeat={Infinity}
+                    cursor={true}
+                    style={
+                        {
+                            whiteSpace: 'pre-line',
+                            display: 'block'
+                        }
+                    }
+                    omitDeletionAnimation={true}
+                />
+            </div>
+        </div>
     </div>
   )
 }
