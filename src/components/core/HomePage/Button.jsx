e9e72518d7c9950ccdde10bf146871d20cab7d59
Tushar <kmrtushar01@gmail.com>
2024-08-18 20:59:08 +0530

    setting css configration on the basis of active state

--- src/components/core/HomePage/Button.jsx
+++ src/components/core/HomePage/Button.jsx
@@ -5,7 +5,7 @@ const CTAbutton = ({ children, linkto, active, shadow }) => {
   return (
     <Link to={linkto}>
         <div className={`text-center text-[13px] px-6 py-3 rounded-md font-bold ${shadow}
-                       ${active? "bg-yellow-50 text-black "  : "bg-richblack-800"}`}>
+                       ${active? "bg-yellow-50 text-richblack-900 "  : "bg-richblack-800 text-richblack-50"}`}>
             {children}
          </div>    
      </Link>
