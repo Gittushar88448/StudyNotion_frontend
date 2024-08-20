0a9d3024acfca7c9af6909f277d8fa6703792363
Tushar <kmrtushar01@gmail.com>
2024-08-20 23:58:48 +0530

    adding custom class property

--- src/components/core/HomePage/HighlightText.jsx
+++ src/components/core/HomePage/HighlightText.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HighlightText = ({text}) => {
   return (
-    <span className='font-semibold mx-[9px] bg-gradient-to-r from-[#1FA2FF] via-[#12D8FA] to-[#A6FFCB] inline-block text-transparent bg-clip-text'>
-        {`${text }`}
+    <span className='font-semibold px-2 highlighted bg-gradient-to-r from-[#1FA2FF] via-[#12D8FA] to-[#A6FFCB] inline-block text-transparent bg-clip-text'>
+        {text}
     </span>
   )
 }
