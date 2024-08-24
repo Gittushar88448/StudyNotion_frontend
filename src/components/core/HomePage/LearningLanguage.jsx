32302e157b62d47b12730cb5e51c2e08eba96b79
Tushar <kmrtushar01@gmail.com>
2024-08-24 20:55:11 +0530

    component of learning section in homepage

--- src/components/core/HomePage/LearningLanguage.jsx
+++ src/components/core/HomePage/LearningLanguage.jsx
@@ -7,24 +7,24 @@ import CTAbutton from './Button'
 
 const LearningLanguage = () => {
   return (
-    <div className='px-20 py-28'>
+    <div className='px-20 py-28 learning-section'>
         <div className='flex flex-col gap-14  items-center'>
 
-            <div className='flex flex-col gap-4 items-center'>
+            <div className='flex flex-col gap-4 md:items-center learning-section-text'>
 
                 <div className='text-4xl font-semibold text-center -tracking-wider'>
                     Your swiss knife for <HighlightText text={"learning any language"}/>
                 </div>
-                <div className='text-center text-richblack-600 mx-auto text-base font-medium w-[70%]'>
+                <div className='text-center text-richblack-600 items-center text-base font-medium md:w-[70%]'>
                     Using spin making learning multiple languages easy. with 20+ languages
                     realistic voice-over, progress tracking, custom schedule and more.
                 </div>
 
             </div>
-            <div className='flex items-center justify-center mt-5'>
-                <img src={know_your_progress} alt='know your progress card' className='object-contain -mr-32'></img>
-                <img src={compare_with_others} alt='compare card' className='object-contain'></img>
-                <img src={plan_your_lesson} alt='plan card' className='object-contain -ml-36'></img>
+            <div className='flex items-center justify-center mt-5 learning-images'>
+                <img src={know_your_progress} alt='know your progress card' className=' learning-images-first object-contain -mr-32'></img>
+                <img src={compare_with_others} alt='compare card' className=' learning-images-second object-contain'></img>
+                <img src={plan_your_lesson} alt='plan card' className=' learning-images-third object-contain -ml-36'></img>
             </div>
 
             <div>
