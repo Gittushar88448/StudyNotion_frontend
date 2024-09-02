d4131977a24f9d5c50ce59a07fccdcb706354572
Tushar <kmrtushar01@gmail.com>
2024-09-02 21:23:06 +0530

    error page

---
+++ src/components/common/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div className='text-white flex justify-center items-center'>
+        <p>Error 404 not found</p>
+    </div>
+  )
+}
+
+export default Error
